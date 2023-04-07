b57905f66c05b6b3196e4937999f23cdb16be702
geng.li <geng.li@timschina.com>
2023-04-07 23:02:15 +0800

    update

--- Demo/src/router/index.ts
+++ Demo/src/router/index.ts
@@ -19,6 +19,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   },
+  {
+    path: '/test',
+    name: 'Test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Test.vue'),
+  },
 ];
 
 const router = createRouter({
