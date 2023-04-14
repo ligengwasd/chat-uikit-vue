2eaf660f0202331c5c44a8df772b02e4488624a8
geng.li <geng.li@timschina.com>
2023-04-15 00:37:38 +0800

    update

--- Demo/src/router/index.ts
+++ Demo/src/router/index.ts
@@ -18,14 +18,13 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
-  },
-  {
-    path: '/test',
-    name: 'Test',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Test.vue'),
+    children: [
+      {
+        path: '/home/chat',  // 项目 进行中
+        name: 'test',
+        component: () => import(/* webpackChunkName: "about" */ '../views/Chat.vue')    // 子路由
+      }
+    ]
   },
 ];
 
