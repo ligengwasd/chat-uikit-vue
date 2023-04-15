32e056b03000ee46fca8632856d129347d53ff6f
geng.li <geng.li@timschina.com>
2023-04-15 12:59:30 +0800

    update

--- Demo/src/views/FormGenerator.vue
+++ Demo/src/views/FormGenerator.vue
@@ -1,13 +1,14 @@
 <template>
-    <vue-form-generator :schema="schema" :model="model" :options="formOptions"></vue-form-generator>
-
+<!--    <vue-form-generator>2222</vue-form-generator>-->
+  表单设计器
 </template>
 <script>
 import VueFormGenerator from "vue-form-generator";
 import "vue-form-generator/dist/vfg.css";
 export default {
-    components: {
-        "vue-form-generator": VueFormGenerator.component
-    }
+    // components: {
+    //     "vue-form-generator": VueFormGenerator.component
+    // }
 };
+
 </script>
